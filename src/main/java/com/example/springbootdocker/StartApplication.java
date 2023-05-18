ee84d7683586dd2984af401c14db643d00c52f20
ShiroNatsu22 <aushojgs@gmail.com>
2023-05-18 15:58:15 +0200

    Added index controller

---
+++ src/main/java/com/example/springbootdocker/StartApplication.java
@@ -0,0 +1,22 @@
+package com.example.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartApplication {
+
+	@RequestMapping("/")
+    public String index() {
+        return "Welcome to Dockerized Spring Boot Application!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
